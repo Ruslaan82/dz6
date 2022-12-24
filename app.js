345fec6fba4380b8b79a26eb17cf64a7320b42d3
Ruslaan82 <abdrakhmanovv82@gmail.com>
2022-12-24 11:41:55 +0600

    'dz6'

---
+++ app.js
@@ -0,0 +1,21 @@
+// const counter = document.querySelectorAll('.counter');
+// const incBtn = document.querySelector('.increment');
+// const decBtn = document.querySelector('.decrement');
+// const input = document.querySelector('input');
+
+const btns=document.querySelectorAll('.counter__btn');
+btns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        const direction = this.dataset.direction;
+        const inp = this.parentElement.querySelector('.counter__value');
+        const currentValue = +inp.value;
+        let  newValue;
+
+        if (direction === 'plus') {
+            newValue = currentValue + 1;
+        } else {
+            newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+        }
+        inp.value = newValue;
+    })
+})
